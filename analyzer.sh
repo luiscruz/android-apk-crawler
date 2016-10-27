75913e522e25470df4092b5bb6cf0d34497a4b38
Luis Cruz <ei06034@fe.up.pt>
2016-10-27 18:10:42 +0100

    added app version, added several other api calls'

--- analyzer.sh
+++ analyzer.sh
@@ -8,9 +8,24 @@ KEYWORDS=(
     "Landroid/app/job/JobInfo/Builder;->build"
     "Lcom/google/android/gms/gcm/PeriodicTask/Builder;->build"
     "Lcom/google/android/gms/gcm/OneoffTask/Builder;->build"
+    "Landroid/app/AlarmManager;->setWindow"
+    "Landroid/app/AlarmManager;->setExact"
+    "Landroid/app/AlarmManager;->setExactAndAllowWhileIdle"
+    "Landroid/app/AlarmManager;->setInexactRepeating"
+    "Landroid/app/AlarmManager;->setRepeating"
+    "Landroid/app/AlarmManager;->set"
+    "Landroid/app/AlarmManager;->setAndAllowWhileIdle"
+    "Landroid/app/AlarmManager;->setAlarmClock"
+    "Landroid/os/Handler;->post"
+    "Landroid/os/Handler;->postDelayed"
+    "Landroid/os/Handler;->sendEmptyMessage"
+    "Landroid/os/Handler;->sendMessage"
+    "Landroid/os/Handler;->sendMessageAtTime"
+    "Landroid/os/Handler;->sendMessageDelayed"
 )
+#FIXME: Handler send methods are probably used in a subclass -> not sure this works
 
-printf "App,Package,AndroidVersion,${KEYWORDS[*]}\n" | tr ' ' ','
+printf "App,Version,Package,AndroidVersion,${KEYWORDS[*]}\n" | tr ' ' ','
 for APK_PATH in $APK_DIR/*.apk; do
     APK_FILENAME="${APK_PATH##*/}"
     PROJECT="${APK_FILENAME%.*}"
@@ -18,6 +33,7 @@ for APK_PATH in $APK_DIR/*.apk; do
     
     printf "$PROJECT"
     apktool d $APK_PATH -o $DECODED_APK &> /dev/null
+    printf ",%s" "$(manifest-reader --version-name --apk $APK_PATH)"
     printf ",%s" "$(manifest-reader --package-name $DECODED_APK/AndroidManifest.xml)"
     printf ",%s" "$(manifest-reader --android-sdk-version $DECODED_APK/AndroidManifest.xml)"
     for KEYWORD in ${KEYWORDS[*]}; do
