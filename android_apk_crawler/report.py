6565b749792a52d7ecc4118df09d93be9a57dd16
Luis Cruz <ei06034@fe.up.pt>
2017-01-25 16:19:00 +0000

    added report script

---
+++ android_apk_crawler/report.py
@@ -0,0 +1,49 @@
+import os
+import os.path
+import sys
+import click
+import time
+import pandas
+
+KEYWORDS = [
+    "Landroid/app/job/JobInfo/Builder;->build",
+    "Lcom/google/android/gms/gcm/PeriodicTask/Builder;->build",
+    "Lcom/google/android/gms/gcm/OneoffTask/Builder;->build",
+    "Landroid/app/AlarmManager;->setWindow",
+    "Landroid/app/AlarmManager;->setExact",
+    "Landroid/app/AlarmManager;->setExactAndAllowWhileIdle",
+    "Landroid/app/AlarmManager;->setInexactRepeating",
+    "Landroid/app/AlarmManager;->setRepeating",
+    "Landroid/app/AlarmManager;->set",
+    "Landroid/app/AlarmManager;->setAndAllowWhileIdle",
+    "Landroid/app/AlarmManager;->setAlarmClock",
+   "Lcom/firebase/jobdispatcher/FirebaseJobDispatcher;->newJobBuilder",
+]
+
+@click.command()
+@click.option(
+    '--input', '-i'
+    prompt="Input CSV files",
+    help='CSV files to be used.')
+@click.option(
+    '--output-dir', '-o',
+    prompt="Output directory",
+    help='Directory where to store the reports.')
+def tool(input, output_dir):
+    """Tool to generate reports resulting from the android-apk-crawler analysis."""
+
+    start_time = time.time()
+
+    df = pandas.read_csv(input)
+    print df[KEYWORDS].sum()
+    df[KEYWORDS].sum().plot(kind="bar")
+    df['minSdkVersion'].hist()
+    
+    click.secho(
+        "Generated reports in {:.1f} minutes.".format(
+            (time.time() - start_time) / 60)
+        fg='blue'
+    )
+
+if __name__ == '__main__':
+    tool()
