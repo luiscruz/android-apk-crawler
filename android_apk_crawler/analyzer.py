c491fe13165387e76406f1402c169790b2f149c3
Luis Cruz <ei06034@fe.up.pt>
2017-01-25 11:23:56 +0000

    added exectime; decodie apks; added short params

--- android_apk_crawler/analyzer.py
+++ android_apk_crawler/analyzer.py
@@ -2,9 +2,10 @@ import os
 import glob
 import os.path
 import sys
-from subprocess import check_output
+import subprocess
 import click
 import yaml
+import time
 
 KEYWORDS = (
     "Landroid/app/job/JobInfo/Builder;->build",
@@ -32,7 +33,7 @@ def printm(msg, file=sys.stdout):
 
 
 def get_version_name(apk):
-    return check_output([
+    return subprocess.check_output([
         "manifest-reader",
         "--version-name",
         "--apk", apk
@@ -40,7 +41,7 @@ def get_version_name(apk):
 
 
 def get_package_name(decoded_apk):
-    return check_output([
+    return subprocess.check_output([
         "manifest-reader",
         "--package-name",
         decoded_apk + "/AndroidManifest.xml"
@@ -48,7 +49,7 @@ def get_package_name(decoded_apk):
 
 
 def get_sdk_version(decoded_apk):
-    return check_output([
+    return subprocess.check_output([
         "manifest-reader",
         "--android-sdk-version",
         decoded_apk + "/AndroidManifest.xml"
@@ -74,7 +75,7 @@ def get_min_sdk_version(decoded_apk):
 
 def get_occurrences(decoded_apk, keywords):
     result = dict.fromkeys(keywords, 0)
-    raw_output = check_output(
+    raw_output = subprocess.check_output(
         "grep -rhoE \"" +
         "|".join(keywords) + "\" " + decoded_apk +
         "| sort | awk '{ print $1 }' | uniq -c", shell=True)
@@ -85,22 +86,36 @@ def get_occurrences(decoded_apk, keywords):
     return result
 
 
+def decode_apk(apk, output_dir):
+    subprocess.check_output(
+        "apktool d {} -o {}".format(apk,output_dir),
+        shell=True,
+    )
+
+
 @click.command()
 @click.option(
     '--apks',
     default="./apks/",
     help='Path to directory with all apks. Defaults to "./apks"')
 @click.option(
-    '--output-file',
+    '--output-file', '-o',
     prompt="Output CSV file",
     help='File to save the CSV output.')
-def tool(apks, output_file):
+@click.option(
+    '--exectime', '-t', is_flag=True,
+    help='print execution time')
+def tool(apks, output_file, exectime):
     """Tool to analyze Android applications through their APKs."""
 
-    with open(output_file, 'a') as f:
-        print >> f, "App,Version,Package,AndroidVersion,minSdkVersion," \
-            + ",".join(KEYWORDS)
-    apk_files = glob.glob(apks + "*.apk")
+    if exectime:
+        start_time = time.time()
+
+    if not os.path.exists(output_file):
+        with open(output_file, 'a') as f:
+            print >> f, "App,Version,Package,AndroidVersion,minSdkVersion," \
+                + ",".join(KEYWORDS)
+    apk_files = glob.glob(apks + "/**.apk")
 
     with click.progressbar(
         apk_files,
@@ -112,7 +127,9 @@ def tool(apks, output_file):
         for file in apk_files_bar:
             filename = os.path.basename(file)
             project = os.path.splitext(filename)[0]
-            decoded_apk = "./tmp/%s" % project
+            decoded_apk = "./tmp/{}".format(project)
+            if not os.path.exists(decoded_apk):
+                decode_apk(file, decoded_apk)
             occurrences = get_occurrences(decoded_apk, KEYWORDS)
             line = ",".join([
                 project,
@@ -125,6 +142,14 @@ def tool(apks, output_file):
             ])
             with open(output_file, 'a') as f:
                 print >> f, line
+
+    if exectime:
+        click.secho(
+            "Processed {} files in {:.1f} minutes.".format(
+                len(apk_files),
+                (time.time() - start_time) / 60),
+            fg='blue'
+        )
 
 
 if __name__ == '__main__':
